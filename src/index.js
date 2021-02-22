02d6daee0cd54de4b1821403e1bfe32b446c8e15
Alexey KHokhlow <lomtik25@gmail.com>
2021-02-22 14:14:38 +0300

    First commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (typeof(array) == 'undefined' || array.length == 0) {
+        return 0
+    }
+    let minNumber = array[0]
+    array.forEach(element => {
+        if (minNumber > element) {
+            minNumber = element
+        }
+    });
+    return minNumber
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (typeof(array) == 'undefined' || array.length == 0) {
+        return 0
+    }
+    let maxNumber = array[0]
+    array.forEach(element => {
+        if (maxNumber < element) {
+            maxNumber = element
+        }
+    });
+    return maxNumber
+
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (typeof(array) == 'undefined' || array.length == 0) {
+        return 0
+    }
+    let sum = 0
+    array.forEach(element => {
+        sum = sum + element
+    })
+
+    return sum / array.length
+
 }
